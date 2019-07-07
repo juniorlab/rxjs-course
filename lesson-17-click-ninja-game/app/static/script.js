2c51069f0ad8fe6de293639de668fbffd4f211ae
Alexey Yunoshev <alexey.yunoshev@gmail.com>
2019-07-07 11:10:51 +0300

    refactor(lesson-17-click-ninja-game)

--- lesson-17-click-ninja-game/app/static/script.js
+++ lesson-17-click-ninja-game/app/static/script.js
@@ -1,5 +1,5 @@
 import {fromEvent} from 'rxjs';
-import {tap, scan, timeInterval} from 'rxjs/operators';
+import {scan, timeInterval} from 'rxjs/operators';
 
 window.addEventListener('load', () => {
   const mainElement = document.getElementById('main');
@@ -8,23 +8,39 @@ window.addEventListener('load', () => {
   const fastestClickElement = document.getElementById('fastestClick');
 
   function updateInterface(score, record, interval) {
-
-    if (record) {
-      recordElement.innerText = record;
-    }
-
-    if (interval) {
-      fastestClickElement.innerText = interval;
-    }
-
+    recordElement.innerText = record;
+    fastestClickElement.innerText = interval;
     outputElement.innerText = score;
     outputElement.style.fontSize = `${score}px`;
     score = score <= 100 ? (score / 100).toFixed(2) : 1;
     mainElement.style.backgroundColor = `rgba(255, 69, 0, ${score})`;
   }
 
-  function reset() {
-    updateInterface(0);
+  function updateState(state, timeInterval, initialLocalState) {
+    const interval = timeInterval.interval;
+
+    if (interval > state.threshold) {
+      // reset temporary state
+      return {
+        ...state,
+        ...initialLocalState,
+      }
+    }
+
+    const score = state.score + 1;
+
+    // update temporary and persistent state
+    return {
+      interval,
+      score,
+      fastestClick: state.fastestClick < interval
+        ? state.fastestClick
+        : interval,
+      record: state.record > score
+        ? state.record
+        : score,
+      threshold: state.threshold - 2,
+    };
   }
 
   const initialLocalState = {
@@ -33,42 +49,18 @@ window.addEventListener('load', () => {
     threshold: 300,
   };
 
+  const initialState = {
+    record: 0,
+    fastestClick: Infinity,
+    ...initialLocalState
+  };
+
   fromEvent(document, 'click')
     .pipe(
       timeInterval(),
       scan((state, timeInterval) => {
-        const interval = timeInterval.interval;
-
-        if (interval > state.threshold) {
-          return {
-            ...state,
-            ...initialLocalState,
-          }
-        }
-
-        const score = state.score + 1;
-
-        return {
-          interval,
-          score,
-          fastestClick: state.fastestClick < interval
-            ? state.fastestClick
-            : interval,
-          record: state.record > score
-            ? state.record
-            : score,
-          threshold: state.threshold - 2,
-        };
-      }, {
-        record: 0,
-        fastestClick: Infinity,
-        ...initialLocalState
-      }),
-      tap((state) => {
-        if (state.score === 0) {
-          reset()
-        }
-      })
+        return updateState(state, timeInterval, initialLocalState);
+      }, initialState),
     )
     .subscribe(
       (state) => updateInterface(state.score, state.record, state.fastestClick),
