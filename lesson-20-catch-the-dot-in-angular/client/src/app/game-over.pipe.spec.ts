9b2fa69f14836f02c58b0eda769c7901f6b89562
Alexey Yunoshev <alexey.yunoshev@gmail.com>
2019-07-11 10:05:02 +0300

    feat(lesson-20-catch-the-dot-game-in-angular): add gameOver pipe

---
+++ lesson-20-catch-the-dot-in-angular/client/src/app/game-over.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GameOverPipe } from './game-over.pipe';
+
+describe('GameOverPipe', () => {
+  it('create an instance', () => {
+    const pipe = new GameOverPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
