0586a5d6d3e7baa464f6030727f68bc895382fd4
Alexey Yunoshev <alexey.yunoshev@gmail.com>
2019-07-26 15:45:21 +0300

    perf(lesson-40-matrix-rain): change innerText to textContent

--- lesson-40-matrix-rain/app/static/script.js
+++ lesson-40-matrix-rain/app/static/script.js
@@ -75,14 +75,14 @@ window.addEventListener('load', () => {
         for (let c = 0; c < numberOfCells; c++) {
           const cell = draft.cells[c];
           if (current.value.interval % cell.interval === 0) {
-            cell.element.textcontent = current.value.data[c].symbol;
+            cell.element.textContent = current.value.data[c].symbol;
           }
         }
       } else {
         for (let c = 0; c < numberOfCells; c++) {
           const cell = draft.cells[c];
           if (current.value.interval % cell.interval === 0) {
-            cell.element.textcontent = current.value.data[c].symbol;
+            cell.element.textContent = current.value.data[c].symbol;
           }
           draft.cells[c].interval = current.value.data[c].interval;
         }
